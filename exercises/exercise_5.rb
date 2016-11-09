5dd073e1a49fb19b2a003864d9a6cc3057e28750
nicadams <nic.adams@hotmail.com>
2016-11-09 02:29:08 +0000

    exercise 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+combined_rev = Store.sum("annual_revenue")
+puts combined_rev
+
+avg_rev = Store.average("annual_revenue")
+puts avg_rev
+
+puts Store.where("annual_revenue > ?", 1000000).count
